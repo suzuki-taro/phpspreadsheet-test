abc99f44255d058f005c825b8a6100feca97b4f2
Suzuki Taro <t.suzuki@systemconcierge.co.jp>
2023-10-20 15:40:19 +0900

    add test1.php

---
+++ test1.php
@@ -0,0 +1,33 @@
+<?php
+
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+require './vendor/autoload.php';
+
+// https://phpspreadsheet.readthedocs.io/en/latest/topics/recipes/#write-a-date-or-time-into-a-cell
+// https://phpspreadsheet.readthedocs.io/en/latest/topics/accessing-cells/#beware-cells-assigned-to-variables-as-a-detached-reference
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->getCell('A1')->setValue('a')->getStyle()->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_YYYYMMDD);
+$sheet->getCell('B1')->setValue('b');
+
+echo "---- foreach ----\n";
+foreach ($sheet->getRowIterator()->current()->getCellIterator() as $cell) {
+    printf("%s\t%s\n", $cell->getValue(), $cell->getStyle()->getNumberFormat()->getFormatCode());
+}
+
+echo "---- EnumeratesValues::each ----\n";
+collect($sheet->getRowIterator()->current()->getCellIterator())->each(function (Cell $cell) {
+    printf("%s\t%s\n", $cell->getValue(), $cell->getStyle()->getNumberFormat()->getFormatCode());
+});
+
+// $php test1.php
+// ---- foreach ----
+// a	yyyy-mm-dd
+// b	General
+// ---- EnumeratesValues::each ----
+// a	General        <-- expected 'yyyy-mm-dd'
+// b	General
